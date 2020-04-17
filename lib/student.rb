a2a51ffc580fac120dbae4be18e7964bdabde4c1
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-17 15:23:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,15 +59,15 @@ def self.students_below_12th_grade
   end
 end
 
-def self.first_X_students_in_grade_10
+def self.first_X_students_in_grade_10(num)
   sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
     LIMIT ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
     end
   end
 
