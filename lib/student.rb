8c5fb8b205bbc365459ff5fc1b6f92158e0f0646
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-17 15:04:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
 
   def self.all
-    sql = <<- SQL
+    sql = <<-SQL
     SELECT * FROM Students
     SQL
 
@@ -23,7 +23,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<- SQL
+    sql = <<-SQL
     SELECT * FROM students
     WHERE name = ?
     LIMIT 1
