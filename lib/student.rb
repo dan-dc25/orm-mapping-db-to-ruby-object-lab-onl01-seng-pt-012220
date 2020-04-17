374310285f40809feee5d409fa16ecb7ba216e5b
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-17 15:32:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,17 @@ def self.first_student_in_grade_10
     end.first
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql, grade).map do |row|
+        self.new_from_db(row)
+      end
+    end
+
 
 
   def save
